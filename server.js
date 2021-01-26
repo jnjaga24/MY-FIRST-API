1ff12c5dd7c29ac288c4e3fa1f80a19c7628c7cd
Jagadeesh <Jnjaga24@gmail.com>
2021-01-26 18:27:05 +0530

    MY-FIRST-API

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const server = http.createServer((req,res) =>{
+    // console.log(req);
+    // console.log(req.method);
+    const {headers,url,method} = req;
+    console.log(headers);
+    console.log(url);
+    console.log(method);
+    res.setHeader('Content-Type','application/json');
+    res.end(JSON.stringify({status:200,message:"My First Api"}));
+});
+const PORT = 5000;
+server.listen(PORT,()=> console.log(`Server running on ${PORT}`));
